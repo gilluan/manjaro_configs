cd024ea6db072b67bb43b906ce894ae6c0dd0c52
Gilluan Formiga <gilluan.formiga@gmail.com>
2020-06-21 14:19:06 -0300

    test

---
+++ setup_manjaro.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo pacman -S git --noconfirm
+
+
+# Installing the google chrome
+git clone https://aur.archlinux.org/google-chrome.git
+
+cd google-chrome
+
+makepkg -s --noconfirm
+
+sudo pacman -U google-chrome*.tar.xz
+
+
+pacman -S nodejs npm --noconfirm
+
+pacman -S emacs ripgrep --noconfirm
+
+
+pacman -S zsh --noconfirm
+
+
+
+
+#
+#echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
+#source ~/.zshrc#echo ".dotfiles.git" >> .gitignore
+#git clone --bare https://www.github.com/gilluan/dotfiles.git $HOME/.dotfiles.git
+#dotfiles checkout
+#dotfiles config --local status.showUntrackedFiles no
+
+# mv ~/.bashrc ~/.bashrc_default
+ # UUUUUUUUU
