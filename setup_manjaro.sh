00d0a063d846991e678e08bb7dbbf3065360aa1e
Gilluan Formiga <gilluan@hotmail.com.br>
2020-06-22 11:04:03 -0300

    atualizando

--- setup_manjaro.sh
+++ setup_manjaro.sh
@@ -17,6 +17,9 @@ pacman -S nodejs npm --noconfirm
 
 pacman -S emacs ripgrep --noconfirm
 
+# optional dependencies
+pacman -S clang tar fd --noconfirm
+
 
 pacman -S zsh --noconfirm
 
@@ -24,11 +27,11 @@ pacman -S zsh --noconfirm
 
 
 #
-#echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
-#source ~/.zshrc#echo ".dotfiles.git" >> .gitignore
-#git clone --bare https://www.github.com/gilluan/dotfiles.git $HOME/.dotfiles.git
-#dotfiles checkout
-#dotfiles config --local status.showUntrackedFiles no
+echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
+source ~/.zshrc#echo ".dotfiles.git" >> .gitignore
+git clone --bare https://www.github.com/gilluan/dotfiles.git $HOME/.dotfiles.git
+dotfiles checkout
+dotfiles config --local status.showUntrackedFiles no
 
-# mv ~/.bashrc ~/.bashrc_default
+ mv ~/.bashrc ~/.bashrc_default
  # UUUUUUUUU
