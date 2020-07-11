87cc78a109b159c434738a8b9c5b89b10f5e7701
Gilluan Formiga <gilluan@hotmail.com.br>
2020-07-11 15:21:16 -0300

    Update setup_manjaro.sh

--- setup_manjaro.sh
+++ setup_manjaro.sh
@@ -10,47 +10,64 @@ cd google-chrome
 
 makepkg -s --noconfirm
 
-sudo pacman -U google-chrome*.tar.xz
+sudo pacman -U google-chrome*.tar.xz --noconfirm
 
 sudo xdg-settings set default-web-browser google-chrome.desktop
 
+cd ..
+rm -rf google-chrome
 
 
 
-pacman -S nodejs npm --noconfirm
 
-pacman -S emacs ripgrep --noconfirm
+
+sudo pacman -S nodejs npm --noconfirm
+
+sudo pacman -S emacs ripgrep --noconfirm
 
 # optional dependencies
-pacman -S clang tar fd --noconfirm
+sudo pacman -S clang tar fd --noconfirm
 
 
-pacman -S zsh --noconfirm
+#pacman -S zsh --noconfirm
 
 
-sudo pacman -S --needed base-devel
+sudo pacman -S --needed base-devel --noconfirm
+
 git clone https://aur.archlinux.org/pikaur.git
 cd pikaur
-makepkg -fsri
+makepkg -fsri --noconfirm
+cd ..
+rm -rf pikaur
+
 
 git clone https://aur.archlinux.org/slack-desktop.git
 cd slack-desktop
-makepkg -si
-
+makepkg -si --noconfirm
+cd ..
+#rm -rf slack-desktop
 
 git clone https://aur.archlinux.org/snapd.git
 cd snapd
-makepkg -si
+makepkg -si --noconfirm
+cd ..
+rm -rf snapd
+
 
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
+
+####### Must to put a sleep time ######
+####### Must to put a sleep time ######
+####### Must to put a sleep time ######
+####### Must to put a sleep time ######
 
 sudo snap install code --classic
 
 sudo snap install zoom-client
 
 
-sudp pacman -S docker docker-compose
+sudo pacman -S docker docker-compose --noconfirm
 
 #curl -L get.rvm.io > rvm-install
 
@@ -63,22 +80,22 @@ sudp pacman -S docker docker-compose
 # unzip -d /tmp ~/Downloads/chromedriver*.zip
 # sudo mv /tmp/chromedriver /usr/local/bin
 
-#
-echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
-source ~/.zshrc
-echo ".dotfiles.git" >> .gitignore
-git clone --bare https://www.github.com/gilluan/dotfiles.git $HOME/.dotfiles.git
-dotfiles checkout
-dotfiles config --local status.showUntrackedFiles no
+########################################## init to review
+#echo 'alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"' >> $HOME/.zshrc
+#source ~/.zshrc
+#echo ".dotfiles.git" >> .gitignore
+#git clone --bare https://www.github.com/gilluan/dotfiles.git $HOME/.dotfiles.git
+#dotfiles checkout
+#dotfiles config --local status.showUntrackedFiles no
 
- mv ~/.bashrc ~/.bashrc_default
-  mv ~/.zshrc ~/.zshrc_default
+ #mv ~/.bashrc ~/.bashrc_default
+ # mv ~/.zshrc ~/.zshrc_default
   
+ ########################################## end
 # for emacs ag counsel  
-  pacman -S the_silver_searcher
+sudo pacman -S the_silver_searcher --noconfirm
 
-
- sudo pacman -S oracle-sqldeveloper
+sudo pacman -S oracle-sqldeveloper --noconfirm
 
 
  # UUUUUUUUU
